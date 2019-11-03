e971f47280e758af1ebecec264bfeec0b1496b66
sumendra12 <sumendrakadek29@gmail.com>
2019-11-03 14:16:56 +0700

    ultrasoniksensor custom

---
+++ 5/5.ino
@@ -0,0 +1,66 @@
+int calibrationTime=5;
+long unsigned int lowIn; 
+long unsigned int pause=2000; 
+boolean lockLow= true; 
+boolean takeLowTime; 
+int pirPin=12;
+int ledPin=6;
+int button=7; 
+int statusKlik=0; 
+
+void setup() {
+  Serial.begin(9600); 
+  pinMode(pirPin, INPUT); 
+  pinMode(ledPin, OUTPUT); 
+  digitalWrite(pirPin, LOW); 
+}
+
+void loop() { 
+  if(digitalRead(button)==LOW){ 
+      switch(statusKlik){ 
+        case 0 :
+            Serial.print("calibrating sensor"); 
+            for(int i=0; i< calibrationTime; i++){ 
+                Serial.print("."); 
+                delay(1000);
+            }
+            // print in serial  
+            Serial.println("done"); 
+            Serial.println("Sensor Active"); 
+            delay(50); 
+            statusKlik=1;
+            break; 
+        case 1 :
+          // when off pirPin
+          if(digitalRead(pirPin)==HIGH){ 
+              digitalWrite(ledPin, HIGH); 
+              if(lockLow){ 
+                  lockLow= false; 
+                  Serial.println("---"); 
+                  Serial.print("motion detected at"); 
+                  Serial.print(millis()/1000); 
+                  Serial.println("sec"); 
+                  delay(50);
+              }
+              takeLowTime= true;  
+          } 
+          // when on pir pin
+          if(digitalRead(pirPin)==LOW){
+            // mematikan lampu
+              digitalWrite(ledPin, LOW); 
+              if(takeLowTime){ 
+                  lowIn= millis(); 
+                  takeLowTime= false;
+              } 
+              if(!lockLow && millis() - lowIn > pause){ 
+                  lockLow= true; 
+                  Serial.print("motion ended at"); 
+                  Serial.print((millis()- pause)/1000); 
+                  Serial.println("sec"); 
+                  delay(50);  
+              }  
+          }
+            
+      }
+  }
+}
